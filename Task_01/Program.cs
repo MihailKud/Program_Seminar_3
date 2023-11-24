0d0bb950522f8b8c5ae2125a59398c098a66ad9c
MihailKud <hobosmich1975@yandex.ru>
2023-11-24 18:49:50 +0300

    Тренировочный коммит по задачам 1 и 2

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ЗАДАНИЕ №1
+Задайте одномерный массив из 10 целых чисел от 1 до 100.
+Найдите количество элементов массива, значения которых лежат в отрезке [20, 90]
+*/
+
+// Инициализируем массив
+int [] massiv = new int[10];
+int i = 0;
+int quantity = 0;
+Random randoms = new Random();
+
+// Заполняем массив случайными целыми числами в диапазоне от 1 до 100
+while (i < 10)
+{
+    massiv[i] = randoms.Next(1, 100);
+    Console.WriteLine(massiv[i]);
+    i = i + 1;
+}
+
+// Подсчитываем количество элементов массива, значения которых лежат в диапазоне [20, 90]
+for (int ii = 0; ii < 10; ii = ii + 1)
+{
+    if ((massiv[ii] >= 20) & (massiv[ii] <= 90)) quantity = quantity + 1; 
+}
+Console.WriteLine($"Количество элементов массива = {quantity}");
---
+++ Task_02/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ЗАДАНИЕ №2
+Задайте массив на 10 целых чисел. Напишите программу, которая определяет 
+количество четных чисел в массиве.
+*/
+
+// Инициализируем массив
+int [] massiv = new int[10];
+int i = 0;
+int quantity = 0;
+Random randoms = new Random();
+
+// Заполняем массив случайными целыми числами в диапазоне от 1 до 100
+while (i < 10)
+{
+    massiv[i] = randoms.Next(1, 100);
+    Console.WriteLine(massiv[i]);
+    i = i + 1;
+}
+// Определяем количество четных чисел в массиве
+for (int ii = 0; ii < 10; ii = ii + 1)
+{
+    if (massiv[ii] % 2 == 0) quantity = quantity + 1; 
+}
+Console.WriteLine($"Количество четных чисел в массиве = {quantity}");
